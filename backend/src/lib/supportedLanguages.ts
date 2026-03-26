ef9e1405626efbaab5c0541491800015a7945765
Tarif24 <tarif24@hotmail.com>
2026-03-26 14:49:13 -0400

    chose to switch from babel which only supports js,jsx.ts and tsx to tree-sitter which has a wider range of supported languages also added a list of supported languages

---
+++ backend/src/lib/supportedLanguages.ts
@@ -0,0 +1,14 @@
+const SUPPORTED_LANGUAGES = {
+    '.ts': 'typescript',
+    '.tsx': 'typescript',
+    '.js': 'javascript',
+    '.jsx': 'javascript',
+    '.py': 'python',
+    '.java': 'java',
+    '.go': 'go',
+    '.rb': 'ruby',
+    '.cpp': 'cpp',
+    '.cs': 'c_sharp',
+};
+
+export default SUPPORTED_LANGUAGES;
